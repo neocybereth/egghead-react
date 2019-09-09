2b29c54789cad924adc2fcd17cbdb82dd90a9765
KRAKEN - Sean Magin <kraken-seanmagin@ip-192-168-1-171.us-west-2.compute.internal>
2019-09-09 04:07:44 +0000

    set up test that render a react component with jest and babel

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,5 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+// import '@testing-library/cleanup-after-each';
+import App from './App';
+
 describe('App', () => {
-    it('Runs and passes', () => {
-        expect(true).toBe(true)
+    it('Renders witout error', () => {
+        render(<App />)
     })
 })
