32b31e7432ea904bf68b0bc70761cc52b2d756cc
KRAKEN - Sean Magin <kraken-seanmagin@ip-192-168-1-171.us-west-2.compute.internal>
2019-09-09 03:40:12 +0000

    target specific browsers with babel-preset-env and the babel polyfilll

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,7 +13,14 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                    presets: [['@babel/preset-env', {
+                        targets: [
+                            'last 2 versions',
+                            'not dead',
+                            'not < 2%',
+                        ],
+                        useBuiltIns: 'entry'
+                    }], '@babel/preset-react'],
                     plugins: [
                         '@babel/plugin-proposal-class-properties',
                         'react-hot-loader/babel'
