caf6b16e142c29a6430f698cbfb76b269495e655
KRAKEN - Sean Magin <kraken-seanmagin@ip-192-168-1-171.us-west-2.compute.internal>
2019-09-09 04:20:06 +0000

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
-const Warning = React.lazy(() => import('./Warning'));
+import React from 'react'
+import { hot } from 'react-hot-loader'
+const Warning = React.lazy(() => import('./Warning'))
 
 class App extends React.Component {
-    state = {
-        count: 0
-    }
+  state = {
+    count: 0
+  }
 
-    render() {
-        const { count } = this.state;
-        return (
-            <div>
-                <h1>Heyyyy World</h1>
-                <h2 className={count > 10 ? 'warning' : null}>
-                    Count: {this.state.count}
-                </h2>
-                <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
-                <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
-                {count > 10 ?
-                    <React.Suspense fallback={null}>
-                       <Warning />
-                    </React.Suspense>
-                 : null}
-            </div>
-        )
-    }
+  render() {
+    const { count } = this.state
+    return (
+      <div>
+        <h1>Heyyyy World</h1>
+        <h2 className={count > 10 ? 'warning' : null}>
+          Count: {this.state.count}
+        </h2>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
+        >
+          -
+        </button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
+      </div>
+    )
+  }
 }
 
-export default hot(module)(App);
+export default hot(module)(App)
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,15 +1,17 @@
-const merge = require("webpack-merge");
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const baseConfig = require('./webpack.config.base');
+const merge = require('webpack-merge')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const baseConfig = require('./webpack.config.base')
 
 module.exports = merge(baseConfig, {
-    mode: 'production',
-    plugins: [new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        reportFilename: 'bundle-analyzer.html'
-    })],
-    externals: {
-        react: 'React',
-        'react-dom': 'ReactDOM'
-    }
+  mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: 'bundle-analyzer.html'
+    })
+  ],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
 })
