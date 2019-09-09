eeea32aec21473689c3580b4c1c97a9c59b565ba
KRAKEN - Sean Magin <kraken-seanmagin@ip-192-168-1-171.us-west-2.compute.internal>
2019-09-09 04:48:40 +0000

    error boundary

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import '@babel/polyfill'
 import './styles.css'
+import DefaultErrorBoundary from './DefaultErrorBoundary'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
   </React.StrictMode>,
   document.getElementById('app')
 )
